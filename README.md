b2d865da606128c626ad84e1007e4b9010fb32fc
amustoe <mustoe@gmail.com>
2020-08-06 17:48:17 -0500

    Added foldPK, a set of functions that also functions as a standalone script for iteratively folding RNAs with ShapeKnots to discover multiple PKs

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RNATools
 Houses the RNATools script started by Gregg and now altered by like everybody. 
+
+# foldPK
+Functions and scripts that wrap around ShapeKnots to fold RNAs with multiple PKs
---
+++ foldPK.py
@@ -0,0 +1,116 @@
+# Anthony Mustoe
+# 2020
+
+import RNAtools2 as RNAtools
+import subprocess, argparse, os
+
+
+
+def writeConstraints(outfile, pairs):
+    """pairs is a list of base paired nucs to restrain to single-stranded
+    for further pk folding""" 
+   
+    # turn pairs into 1D list of nts
+    nts = [i for p in pairs for i in p]
+    nts.sort()
+
+    with open(outfile, 'w') as out:
+
+        out.write('DS:\n-1\nSS:\n')
+        
+        for n in nts:
+            out.write("{}\n".format(n))
+        out.write('-1\nMod:\n-1\nPairs:\n-1 -1\nFMN:\n-1\nForbids:\n-1 -1\n')
+
+ 
+
+def runShapeKnots(ShapeKnotsPath, seqfile, output, dmsfile=None, bpfile=None, consfile=None, cordero=False):
+
+    commands = [ShapeKnotsPath, '-m','1']
+    if dmsfile is not None and not cordero:
+        commands.extend(['-dmsnt',dmsfile])
+    elif dmsfile is not None and cordero:
+        commands.extend(['-dms',dmsfile])
+    if bpfile is not None:
+        commands.extend(['-x', bpfile])
+    if consfile is not None:
+        commands.extend(['-c', consfile])
+    
+    commands.extend([seqfile, output])
+
+    print(commands)
+    subprocess.call(commands)
+
+
+
+
+def iterativeShapeKnots(ShapeKnotsPath, seqfile, outprefix, dmsfile=None, bpfile=None, maxPKs=5, cordero=False):
+
+    modelnumber = 0
+    hasPK = True
+    pkpairs = []
+    
+    consfile = None
+    consfilename = '{}.cons'.format(outprefix)
+
+    while hasPK and modelnumber < maxPKs:
+        
+        modelnumber += 1
+        
+        if len(pkpairs) == 0:
+            consfile = None
+        else:
+            writeConstraints(consfilename, pkpairs)
+            consfile = consfilename
+
+        output = '{0}.{1}.ct'.format(outprefix, modelnumber)
+        runShapeKnots(ShapeKnotsPath, seqfile, output, dmsfile=dmsfile, bpfile=bpfile, 
+                      consfile=consfile)
+
+        ct = RNAtools.CT(output)
+        pk = ct.extractPK()
+        
+        if pk[0] is None:
+            hasPK = False
+        else: 
+            print("PK found! {}".format(pk[0]))
+            pkpairs.extend(pk[0])  
+        
+
+    # add pairs back in
+    if len(pkpairs)>0:
+        ct.addPairs(pkpairs)
+        ct.writeCT('{}.f.ct'.format(outprefix))
+    else:
+        subprocess.call(['mv', output, '{}.f.ct'.format(outprefix)])
+
+
+
+
+
+if __name__ == '__main__':
+    
+    prs = argparse.ArgumentParser(description='Script to iteratively run ShapeKnots to find multiple PKs')
+    prs.add_argument('seqfile', help='molecule name')
+    prs.add_argument('outprefix', help="prefix to append to ct files. Note that .f.ct is the 'final' fold")
+    prs.add_argument('--dmsfile', help='dms reactivity file')
+    prs.add_argument('--bpfile', help='Pairmapper bp restraint file')
+    prs.add_argument('--skpath', help='Path to ShapeKnots executable. If not defined, will try and use internally defined path. You can also modify this internal path if desired (will have to edit within code)')
+    args = prs.parse_args()
+    
+
+    # default path to ShapeKnots. Change to path on your Machine
+    defaultpath = '/Users/anthonymustoe/Code/RNAstructure/exe/ShapeKnots'
+     
+
+    if args.skpath is not None and not os.path.isfile(args.skpath):
+        exit('User-provided path to ShapeKnots is invalid! File does not exist')
+    elif args.skpath is None:
+        args.skpath = defaultpath
+        if not os.path.isfile(args.skpath):
+            exit('Default path to ShapeKnots {} is invalid! File does not exist'.format(args.skpath))
+
+
+    iterativeShapeKnots(args.skpath, args.seqfile, args.outprefix, dmsfile=args.dmsfile, bpfile=args.bpfile)
+    
+    
